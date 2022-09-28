814de89d82cd09d92a7c19fbd2024e05cac41949
kallyane-nascimento16 <110573013+kallyane-nascimento16@users.noreply.github.com>
2022-09-28 18:50:22 -0300

    Add files via upload

---
+++ poo_01/poo_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poo_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           aluno alunoobj = new aluno();
+            Console.WriteLine("qual o seu nome?");
+            alunoobj.nome=Console.ReadLine();
+
+            Console.WriteLine("qual sua idade?");
+            alunoobj.idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("qual seu CPF?");
+            alunoobj.cpf = double.Parse(Console.ReadLine());
+
+
+            
+        }
+    }
+}
---
+++ poo_01/poo_1/conteudo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poo_1
+{
+    internal class conteudo
+    {
+        double datames1;
+        double datames2;
+        double nota1, nota2;
+
+        
+
+    }
+}
